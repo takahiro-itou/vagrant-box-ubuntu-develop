207010385c125c5e168edea8f0eb93d13e5b8b79
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-03-09 18:30:19 +0900

    リファクタリング

--- vagrant/provision.root.sh
+++ vagrant/provision.root.sh
@@ -9,13 +9,29 @@ sudo  apt  update
 export DEBIAN_FRONTEND=noninteractive
 sudo  -E  apt  upgrade -y
 
-sudo  apt  install -y  build-essential                          \
-    cmake  doxygen  emacs  git  mercurial                       \
-    autoconf  automake   clang  gcc  g++                        \
-    libcppunit-dev  libtool  ncurses-dev                        \
-    make  time  sudo                                            \
-    language-pack-ja-base  language-pack-ja                     \
-    fcitx  fcitx-mozc  ibus-mozc                                \
+sudo  apt  install -y       \
+    autoconf                \
+    automake                \
+    build-essential         \
+    clang                   \
+    cmake                   \
+    doxygen                 \
+    emacs                   \
+    g++                     \
+    gcc                     \
+    git                     \
+    libcppunit-dev          \
+    libtool                 \
+    mercurial               \
+    ncurses-dev             \
+    make                    \
+    time                    \
+    sudo                    \
+                            \
+    language-pack-ja-base   \
+    language-pack-ja        \
+    fcitx                   \
+    fcitx-mozc  ibus-mozc   \
     ;
 
 # clean up
